30dd1b6d4f3680fdae52c4e2c20415fad96b6ec6
ejlef <feljemenor236@gmail.com>
2025-04-11 14:21:04 +0800

    First Push

---
+++ js/header&drawer-menu.js
@@ -0,0 +1,47 @@
+function toggleIcon() {
+  const icon = document.getElementById("menuIcon");
+  const sidepanel = document.getElementById("mySidepanel");
+
+  if (icon.classList.contains("fa-bars")) {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-times");
+    sidepanel.style.width = "100%";
+    sidepanel.style.height = "90%";
+    document.body.style.overflow = "hidden";
+  } else {
+    icon.classList.remove("fa-times");
+    icon.classList.add("fa-bars");
+    sidepanel.style.width = "0";
+    // sidepanel.style.height = "0";
+    document.body.style.overflow = "auto";
+  }
+}
+
+let lastScrollTop = 0;
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop === 0) {
+    // At the top of the page
+    header.style.position = "static";
+    header.style.boxShadow = "none";
+    header.classList.remove("hidden");
+  } else if (scrollTop > lastScrollTop) {
+    // Scrolling down
+    header.style.position = "static";
+    header.classList.add("hidden");
+  } else {
+    // Scrolling up
+    header.style.position = "sticky";
+    header.style.top = "0";
+    header.style.backgroundColor = "white";
+    header.style.boxShadow = "0px 2px 5px  rgba(0, 0, 0, 0.3)";
+    header.classList.remove("hidden");
+  }
+
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
+
+console.log("Navbar Is Working!");
